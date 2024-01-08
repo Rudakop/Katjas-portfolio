ef1aa6b1ec94f84f5cbb65e767855d7584959a0c
kms.spb <katya_rad@mail.ru>
2024-01-08 19:58:29 +0200

    contact form css

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -83,6 +83,7 @@ const Contact = () => {
                                 <li>
                                     <textarea
                                         placeholder="Message"
+                                        type="text"
                                         name="message"
                                         required
                                     ></textarea>
